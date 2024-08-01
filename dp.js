682401ac8e1713a7c3d0209ccd9abb728f930806
FrancoDalla <157550038+FrancoDalla@users.noreply.github.com>
2024-08-01 01:26:26 -0300

    Actualización
    Actualice el codigo. 
    Se esta tratando de tomar los span que tienen un caracter $ (en teoría, los precios) y colorearlos en caso de que no sean hidden y agregar los hidden a una colección.

--- dp.js
+++ dp.js
@@ -1,1 +1,39 @@
 console.log("HOLA ESTO ESTA ANDANDO :)");
+const spans = document.getElementsByTagName("span");
+let hiddenSpans = [];
+const precios= [];
+
+for(let i=0;i<spans.length;i++)
+{
+
+    let actualSpan = spans[i];
+    if(actualSpan.textContent.includes('$'))
+    {
+
+        if(actualSpan.style.visibility.valueOf === 'hidden')
+        {
+            hiddenSpans.push(actualSpan);
+            actualSpan.style.backgroundColor = 'green';
+        }
+        else
+        {
+            actualSpan.style.backgroundColor = 'yellow';
+            precios.push(actualSpan);
+         }
+
+    }
+}
+
+console.log("Spans ocultos:_ ",hiddenSpans);
+console.log("spans", precios);
+
+for(let i = 0;i<hiddenSpans.length;i++){
+    console.log(hiddenSpans[i].textContent);
+    console.log(hiddenSpans[i].style.visibility.valueOf);
+}
+
+for(let i = 0;i<precios.length;i++){
+    console.log(precios[i].textContent);
+    console.log(precios[i].style.getPropertyValue);
+}
+
