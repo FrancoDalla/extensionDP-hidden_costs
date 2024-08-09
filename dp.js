a9052653a746e293062a86f6aa9df540c7b81daa
FrancoDalla <157550038+FrancoDalla@users.noreply.github.com>
2024-08-09 17:56:34 -0300

    cambio a la forma de deteccion de precios
    Se cambio la manera en la que se detectan los precios ya que, con el uso de includes, se estaba tomando cualquier span que contuviera un $. Con esta nueva forma de comparar, se espera, que se detecten solamente elementos span en los que haya un precio.

--- dp.js
+++ dp.js
@@ -2,19 +2,19 @@ console.log("HOLA ESTO ESTA ANDANDO :)");
 const spans = document.getElementsByTagName("span");
 const hiddenPrecios = [];
 const precios= [];
+const reNumber = /[$]\s*\d+/;
 
 for(let i=0;i<spans.length;i++)
 {
 
     let actualSpan = spans[i];
-    if(actualSpan.textContent.includes('$'))
+    if(reNumber.test(actualSpan.textContent))
     {
 
         if(!actualSpan.checkVisibility())
         {
             hiddenPrecios.push(actualSpan);
             actualSpan.style.backgroundColor = 'green';
-            console.log("SE ENTRO");
         }
         else
         {
@@ -25,15 +25,5 @@ for(let i=0;i<spans.length;i++)
     }
 }
 
-console.log("Spans ocultos:_ ",hiddenPrecios);
-console.log("spans", precios);
-
-for(let i = 0;i<hiddenPrecios.length;i++){
-    console.log(hiddenPrecios[i].textContent);
-}
-console.log("AHORA PRECIOS");
-for(let i = 0;i<precios.length;i++){
-    console.log(precios[i].textContent);
-}
 
 
