9c5b6e25296ce9fadbed71d4545ea3be4a7e7bb1
FrancoDalla <157550038+FrancoDalla@users.noreply.github.com>
2024-09-17 01:22:45 -0300

    Implementación de comparación por distancia
    Se implemento lo descrito. Ademas se quitaron cosas que parecian no ser muy utiles. Entre ellas:
    >Comparacion por elementos catalogados como precios no visibles (osea los catalogados como hidden)
    >Se busca detectar una lista de precios potenciales. Esto teniendo en cuenta paginas donde se muestran varios elementos.
    >

--- hiddenCosts.js
+++ hiddenCosts.js
@@ -1,5 +1,3 @@
-//Todavia no se encuentra en uso. El plan es que con estos modulos se pueda detectar los precios pequeños alrededor del precio que tenga mayor tamaño 
-
 function getCenter(element) {
     const rectangle = element.getBoundingClientRect();
     return {"x": (rectangle.left + rectangle.width) / 2, "y": (rectangle.top + rectangle.height) / 2};
@@ -11,92 +9,74 @@ function getDistance (element,anotherElement) {
     return Math.sqrt(Math.pow(centerA.x - centerB.x, 2) + Math.pow(centerA.y - centerB.y, 2));
 }
 
-/* 
-    FALTA TEST. POSIBLE SOLUCION. INEFICIENTE. 
-
-    se plantea un vector boolean a la par de spans para llevar cuenta de todos los precios y sus
-    cercanos. Por cada precio se marca como ya visitado y en caso de tener otro a una distancia
-    menor a la minima se lo marca tambien ya que podria ser un HiddenCost.
-    como nota aparte:
-    En la mayoria de los casos el precio cercano deberia ser menor al actual, por lo que podria
-    ser marcado por el check de tamaños antes de que llegue al check de distancias.
-*/
-
 console.log("HOLA ESTO ESTA ANDANDO :)");
-const spans = document.getElementsByTagName("span");
-const hiddenCosts = [];
-const prices = [];
+const spans = document.getElementsByTagName("p"); //Esto es temporal porque podrían aparecer precios con varios tipos de tags HTML
+let hiddenCosts = [];
+let prices = [];
 const reNumber = /[$]\s*\d+/;
 const defaultFontSize = 5;
-const minimunDistance = 2; //A determinar
-const adyacents = new Array(spans.length).fill(true); //vector boolean
-let biggestPrice;
+let potentialPrincipalPrices = [];
 let biggestPriceSize = -1;
+const minDistance = 40;
+
+/*Se busca diferenciar precios por el mayor tamaño porque, suponemos, que es mas probable que sean precios finales debido a que llaman más la atención
+ESTO ES LO OPUESTO DE LO QUE ES UN HIDDEN COST.
+Ademas de eso se busca diferenciar precios de por si mediante el uso de RegExp*/
+
 for(let i=0;i<spans.length;i++)
 {
     let actualSpan = spans[i];
     let actualSize = parseInt(window.getComputedStyle(actualSpan).fontSize);  
-    if((reNumber.test(actualSpan.textContent)) && (adyacents[i]))
-        /* 
-           si adyacents[i] devuelve false es porque ya fue detectado como HiddenCost en otra
-           iteracion y no vale la pena revisarlo
-        */
+    if(reNumber.test(actualSpan.textContent))
     {
-
-        if(!actualSpan.checkVisibility())
+        if(actualSize >= biggestPriceSize)
         {
-            hiddenCosts.push(actualSpan);
-            adyacents[i]=false;
-        }
-        else
-        { 
-            if(actualSize < defaultFontSize)
+            if(actualSize > biggestPriceSize)
             {
-                adyacents[i]=false;
-                hiddenCosts.push(actualSpan);
-                actualSpan.style.backgroundColor = 'green';
+                biggestPriceSize = actualSize;
+                prices = prices.concat(potentialPrincipalPrices);
+                potentialPrincipalPrices = [];
+                potentialPrincipalPrices.push(actualSpan);
             }
             else
-                {
-                let distance; 
-                let j = 0;
-                let ok = true;
-                while((ok)&&(j<=spans.length)){
-                    if((j!=i)&&(adyacents[j])){ //si no es el actual y no fue visitado ya
-                        distance=getDistance (actualSpan,spans[j]);
-                        if(distance<minimunDistance){ //si la distancia es menor a la minima
-                            ok=false;
-                            adyacents[j]=false;//visitados el actual y el cercano HiddenCost
-                            adyacents[i]=false;
-                            hiddenCosts.push(actualSpan);
-                            //actualSpan.style.backgroundColor = 'green';
-                        }
-                    j++;
-                }
-                
-                if(adyacents[i]==true){
-                    adyacents[i]=false;//Ya se visito y se analizo si tiene cercanos
-                    prices.push(actualSpan);
-                    actualSpan.style.backgroundColor = 'yellow';
-                    if(actualSize > biggestPriceSize)
-                    {
-                        biggestPriceSize = actualSize;
-                        biggestPrice = actualSpan;
-                    }
-                }
+            {
+                potentialPrincipalPrices.push(actualSpan);
             }
+        }
+        else
+        {
+            prices.push(actualSpan);
         }
     }
 }
+
+/* Se realiza una comparación con los precios que se consideraron no finales buscando diferenciar los que pueden catalogarse como hidden costs*/
+for(let i=0;i<prices.length;i++)
+{
+    let j = 0;
+    while(j < potentialPrincipalPrices.length)
+    {
+        let distance = getDistance(prices[i],potentialPrincipalPrices[j]);
+        if(distance < minDistance )
+        {
+            hiddenCosts.push(prices[i]);
+        }
+        j++;
+    }
 }
-console.log("precios visibles:");
+
+console.log("precios: ");
 for(let i=0; i<prices.length;i++){
     console.log(prices[i].textContent);
 }
 
-console.log("precios ocultos:");
+console.log("precios ocultos: ");
 for(let i=0; i<hiddenCosts.length;i++){
     console.log(hiddenCosts[i].textContent);
 }
 
-console.log(`Tamaño del precio mas grande: ${biggestPriceSize}`);
+console.log(`Tamaño del precio potencial: ${biggestPriceSize}`);
+for(let i=0;i<potentialPrincipalPrices.length;i++)
+    console.log(potentialPrincipalPrices[i].textContent);
+
+
