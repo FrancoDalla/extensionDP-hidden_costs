b6f2d35d4ba6caf6804898b9ade211ab1c96078b
TCVM <44953759+TCVM@users.noreply.github.com>
2024-09-24 00:58:05 -0300

    Add files via upload

--- hiddenCosts.js
+++ hiddenCosts.js
@@ -9,6 +9,74 @@ function getDistance (element,anotherElement) {
     return Math.sqrt(Math.pow(centerA.x - centerB.x, 2) + Math.pow(centerA.y - centerB.y, 2));
 }
 
+function distanceCheck(){
+    for(let i=0;i<prices.length;i++)
+        {
+            let j = 0;
+            while(j < principalPrices.length)
+            {
+                let distance = getDistance(prices[i],principalPrices[j]);
+                if(distance < hiddenCostDistance )
+                {
+                    hiddenCosts.push(prices[i]);
+                    break;
+                }
+                j++;
+            }
+        }
+}
+
+function sizeCheck(){
+    for(let i=0;i<elementos.length;i++)
+        {
+            let actualElement = elementos[i];
+            let actualSize = parseInt(window.getComputedStyle(actualElement).fontSize);  
+            if(reNumber.test(actualElement.textContent))
+            {
+                if(actualSize >= biggestPriceSize)
+                {
+                    if(actualSize > biggestPriceSize)
+                    {
+                        biggestPriceSize = actualSize;
+                        prices = prices.concat(principalPrices);
+                        principalPrices = [];
+                        principalPrices.push(actualElement);
+                    }
+                    else
+                    {
+                        principalPrices.push(actualElement);
+                    }
+                }
+                else
+                {
+                    prices.push(actualElement);
+                }
+            }
+        }
+}
+
+function logPrint(){
+    console.log("precios: ");
+    for(let i=0; i<prices.length;i++){
+        console.log(prices[i].textContent);
+    }
+    
+    console.log("precios ocultos: ");
+    for(let i=0; i<hiddenCosts.length;i++){
+        console.log(hiddenCosts[i].textContent);
+        hiddenCosts[i].setAttribute("style","background-color: #FFCCCB;")
+    }
+    
+    console.log(`Tamaño de precio potencial: ${biggestPriceSize}`);
+    console.log(`Se detectaron ${principalPrices.length} precios potenciales`);
+    
+    for(let i=0;i<principalPrices.length;i++)
+    {
+        console.log(principalPrices[i].textContent);
+        principalPrices[i].setAttribute("style","background-color: #CCFEFF;")
+    }
+}
+
 console.log("HOLA ESTO ESTA ANDANDO :)");
 const elementos = document.querySelectorAll('p,span,h5'); //Esto es temporal porque podrían aparecer precios con varios tipos de tags HTML. Estamos viendo como incluir distintos tags
 let hiddenCosts = [];
@@ -18,68 +86,26 @@ const hiddenCostSize = 5;
 let principalPrices = [];
 let biggestPriceSize = -1;
 const hiddenCostDistance = 40; //El minimo se basa en la pagina de ejemplo pero de momento no pensamos que considerar para este valor.
+
 /*Se busca diferenciar precios por el mayor tamaño porque, suponemos, que es mas probable que sean precios finales debido a que llaman más la atención
 Ademas de eso se busca diferenciar precios de por si mediante el uso de RegExp*/
 
-for(let i=0;i<elementos.length;i++)
-{
-    let actualElement = elementos[i];
-    let actualSize = parseInt(window.getComputedStyle(actualElement).fontSize);  
-    if(reNumber.test(actualElement.textContent))
-    {
-        if(actualSize >= biggestPriceSize)
-        {
-            if(actualSize > biggestPriceSize)
-            {
-                biggestPriceSize = actualSize;
-                prices = prices.concat(principalPrices);
-                principalPrices = [];
-                principalPrices.push(actualElement);
-            }
-            else
-            {
-                principalPrices.push(actualElement);
-            }
-        }
-        else
-        {
-            prices.push(actualElement);
-        }
-    }
-}
+sizeCheck();
 
 /* Se realiza una comparación con los precios que se consideraron no finales buscando diferenciar los que pueden catalogarse como hidden costs*/
-for(let i=0;i<prices.length;i++)
-{
-    let j = 0;
-    while(j < principalPrices.length)
-    {
-        let distance = getDistance(prices[i],principalPrices[j]);
-        if(distance < hiddenCostDistance )
-        {
-            hiddenCosts.push(prices[i]);
-            break;
-        }
-        j++;
-    }
-}
+distanceCheck();
 
-console.log("precios: ");
-for(let i=0; i<prices.length;i++){
-    console.log(prices[i].textContent);
-}
+/* Se imprime resultados para testear */
 
-console.log("precios ocultos: ");
-for(let i=0; i<hiddenCosts.length;i++){
-    console.log(hiddenCosts[i].textContent);
-    hiddenCosts[i].setAttribute("style","background-color: #FFCCCB;")
-}
+logPrint();
 
-console.log(`Tamaño de precio potencial: ${biggestPriceSize}`);
-console.log(`Se detectaron ${principalPrices.length} precios potenciales`);
-
-for(let i=0;i<principalPrices.length;i++)
-{
-    console.log(principalPrices[i].textContent);
-    principalPrices[i].setAttribute("style","background-color: #CCFEFF;")
-}
+// Escuchar el cambio de estado del historial (navegación sin recargar)
+window.addEventListener('popstate', function() {
+    // Lógica que se activa cuando cambia la "ruta" en la SPA
+    sizeCheck();
+    distanceCheck();
+    logPrint();
+    
+    //ESTA NO ES LA UNICA OPCION.
+    // No estoy seguro si es necesario re-analizar toda la pagina una vez que pasa el cambio. Falta probar.
+});
